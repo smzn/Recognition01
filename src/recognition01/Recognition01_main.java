b7c478098596cc6283ce5c190928522506998f29
mizuno <mizuno@133.88.126.25>
2018-05-24 12:27:48 +0900

    20180524

--- src/recognition01/Recognition01_main.java
+++ src/recognition01/Recognition01_main.java
@@ -2,7 +2,10 @@ package recognition01;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ibm.watson.developer_cloud.visual_recognition.v3.VisualRecognition;
 import com.ibm.watson.developer_cloud.visual_recognition.v3.model.DetectFacesOptions;
 import com.ibm.watson.developer_cloud.visual_recognition.v3.model.DetectedFaces;
@@ -13,7 +16,7 @@ public class Recognition01_main {
 		// TODO Auto-generated method stub
 		
 		VisualRecognition service = new VisualRecognition("2018-03-19");
-		service.setApiKey("naisyo");
+		service.setApiKey("1500510b261ca57f54c250b64228b0f3918bf832");
 
 		DetectFacesOptions detectFacesOptions = null;
 		try {
@@ -26,7 +29,28 @@ public class Recognition01_main {
 		}
 		DetectedFaces result = service.detectFaces(detectFacesOptions).execute();
 		System.out.println(result);
-
+		
+		String s = String.valueOf(result);
+		
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			JsonNode node = mapper.readTree(s);
+			int age_min = node.get("images").get(0).get("faces").get(0).get("age").get("min").asInt();
+            System.out.println("age_min : " + age_min);
+            int age_max = node.get("images").get(0).get("faces").get(0).get("age").get("max").asInt();
+            System.out.println("age_max : " + age_max);
+            double age_score = node.get("images").get(0).get("faces").get(0).get("age").get("score").asDouble();
+            System.out.println("age_score : " + age_score);
+            
+            String gender = node.get("images").get(0).get("faces").get(0).get("gender").get("gender").asText();
+            System.out.println("Gender : " + gender);
+            double gender_score = node.get("images").get(0).get("faces").get(0).get("gender").get("score").asDouble();
+            System.out.println("Gender_score : " + gender_score);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 
 }
